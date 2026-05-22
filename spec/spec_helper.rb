1838fd7924027f25d9f4b17d97edbdc18e25d699
Monique Rio <mrio@umich.edu>
2026-05-22 13:15:11 -0400

    remove old_stub spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -146,37 +146,6 @@ end
   end
 end
 
-[:get, :post, :delete].each do |name|
-  define_method(:"old_stub_alma_#{name}_request") do |url:, body: "{}", status: 200, query: {}, no_return: nil|
-    req = stub_request(name, "#{ENV["ALMA_API_HOST"]}/almaws/v1/#{url}").with(
-      headers: {
-        :accept => "application/json",
-        :Authorization => "apikey #{ENV["ALMA_API_KEY"]}",
-        "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-        "User-Agent" => "Ruby"
-      },
-      query: query
-    )
-    req.to_return(body: body, status: status, headers: {content_type: "application/json"}) if no_return.nil?
-    req
-  end
-end
-
-def old_stub_alma_put_request(url:, input:, output:, status: 200, no_return: nil)
-  req = stub_request(:put, "#{ENV["ALMA_API_HOST"]}/almaws/v1/#{url}").with(
-    body: input,
-    headers: {
-      :accept => "application/json",
-      :Authorization => "apikey #{ENV["ALMA_API_KEY"]}",
-      "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-      "User-Agent" => "Ruby",
-      "Content-Type" => "application/json"
-    }
-  )
-  req.to_return(body: output, status: status, headers: {content_type: "application/json"}) if no_return.nil?
-  req
-end
-
 def stub_illiad_get_request(url:, body: "{}", status: 200, query: nil, no_return: nil)
   req = stub_request(:get, "#{ENV["ILLIAD_API_HOST"]}/ILLiadWebPlatform/#{url}").with(
     headers: {
